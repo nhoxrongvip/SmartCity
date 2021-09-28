936e0a04f946249b601222d7a074f9716f96306c
thanhphatlam <tee.thanhlam@gmail.com>
2021-09-27 21:20:37 -0400

    Update Air Quality Fragment

--- SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
@@ -15,15 +15,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AirQualityFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AirQualityFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -35,15 +30,7 @@ public class AirQualityFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AirQualityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static AirQualityFragment newInstance(String param1, String param2) {
         AirQualityFragment fragment = new AirQualityFragment();
         Bundle args = new Bundle();
