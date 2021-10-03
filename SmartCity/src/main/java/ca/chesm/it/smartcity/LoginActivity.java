ba8438d331b23ab47cb716afbf1fad54bdc7c227
Danny <danks76429@gmail.com>
2021-10-03 13:53:49 -0400

    Test login with username admin, password admin to change activity commit.

--- SmartCity/src/main/java/ca/chesm/it/smartcity/LoginActivity.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/LoginActivity.java
@@ -2,13 +2,45 @@ package ca.chesm.it.smartcity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
+    Button btn;
+    EditText txtedituser,txteditpass;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        resg();
+
+        btn.setOnClickListener(view ->
+        {
+            String username,password;
+            username = txtedituser.getText().toString();
+            password = txteditpass.getText().toString();
+            if(username.equals("admin") && password.equals("admin"))
+            {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+
+            }
+        });
+    }
+
+    public void resg()
+    {
+        txtedituser = (EditText) findViewById(R.id.txteditusername);
+        txteditpass = (EditText) findViewById(R.id.txteditpassword);
+        btn = (Button) findViewById(R.id.bntlogin);
     }
 }
