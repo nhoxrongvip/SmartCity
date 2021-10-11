2a2ed6056128e737e431af93626aa84d9721ebf7
thanhphatlam <tee.thanhlam@gmail.com>
2021-10-11 16:57:31 -0400

    Change permission to Setting fragment

--- SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
@@ -26,70 +26,21 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class AirQualityFragment extends Fragment {
 
-    private int STORAGE_PERMISSION_CODE = 1;
-    private View v;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AirQualityFragment() {
-        // Required empty public constructor
-    }
-
-
-    public static AirQualityFragment newInstance(String param1, String param2) {
-        AirQualityFragment fragment = new AirQualityFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        v = inflater.inflate(R.layout.fragment_air_quality, container, false);
-        Button btn = v.findViewById(R.id.bntpremission);
-        btn.setOnClickListener(view ->
-        {
-            if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            {
-                Snackbar.make(getContext(),getView(),"Permission Granted", Snackbar.LENGTH_SHORT).show();
-            }
-            else if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
-                Snackbar.make(getContext(),getView(),"Permission Denied", Snackbar.LENGTH_SHORT).show();
 
-            }
-            else
-            {
-                if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION))
-                {
 
-                }
-                else {
-                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},STORAGE_PERMISSION_CODE);
-                }
-            }
-        });
-
-        return v;
+        return inflater.inflate(R.layout.fragment_air_quality, container, false);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
 
-        }
     }
 
 }
--- SmartCity/src/main/java/ca/chesm/it/smartcity/AppSettingFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/AppSettingFragment.java
@@ -1,64 +1,54 @@
 package ca.chesm.it.smartcity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AppSettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.snackbar.Snackbar;
+
+
 public class AppSettingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int STORAGE_PERMISSION_CODE = 1;
+    private View v;
 
-    public AppSettingFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AppSettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AppSettingFragment newInstance(String param1, String param2) {
-        AppSettingFragment fragment = new AppSettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+        v = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button btn = v.findViewById(R.id.bntpremission);
+        btn.setOnClickListener(view ->
+        {
+            if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                Snackbar.make(getContext(),getView(),"Permission Granted", Snackbar.LENGTH_SHORT).show();
+            }
+
+            else
+            {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION))
+                {
+                    Snackbar.make(getContext(),getView(),"Permission Denied", Snackbar.LENGTH_SHORT).show();
+                }
+                else {
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},STORAGE_PERMISSION_CODE);
+                }
+            }
+        });
+        return v;
     }
 }
