bd8da951032a3675d3b9c043218226cc2e92e374
thanhphatlam <tee.thanhlam@gmail.com>
2021-11-11 17:59:11 -0500

    Update layout

--- SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/AirQualityFragment.java
@@ -16,29 +16,70 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.google.android.material.snackbar.Snackbar;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class AirQualityFragment extends Fragment {
+
+    View v;
+
+    //AQI report components
+    TextView location, aqi_quality, aqi_condition;
+    CircleImageView aqi_conditionemotion;
+
+    //Pollutant components
+    TextView pollutants_pm25, pollutants_co2, pollutants_o3;
+
+    //Daily graph components
+    RadioButton rb_aqi, rb_co, rb_co2, rb_o3, rb_pm25, rb_so2, rb_no2;
+    BarChart dailygraph;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_air_quality, container, false);
 
 
-        return inflater.inflate(R.layout.fragment_air_quality, container, false);
+        return v;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getID();
+    }
+
+    private void getID(){
+        location = v.findViewById(R.id.AQ_currentlocation);
+        aqi_quality = v.findViewById(R.id.AQ_quality);
+        aqi_condition = v.findViewById(R.id.AQ_condition);
+        aqi_conditionemotion = v.findViewById(R.id.AQ_conditionemotion);
+
+        pollutants_pm25 = v.findViewById(R.id.AQ_pm25);
+        pollutants_co2 = v.findViewById(R.id.AQ_co2);
+        pollutants_o3 = v.findViewById(R.id.AQ_o3);
+
+        rb_aqi = v.findViewById(R.id.rb_aqi);
+        rb_co = v.findViewById(R.id.rb_co);
+        rb_co2 = v.findViewById(R.id.rb_co2);
+        rb_o3 = v.findViewById(R.id.rb_o3);
+        rb_pm25 = v.findViewById(R.id.rb_pm25);
+        rb_so2 = v.findViewById(R.id.rb_so2);
+        rb_no2 = v.findViewById(R.id.rb_no2);
+        dailygraph = v.findViewById(R.id.AQ_dailygraph);
 
 
     }
