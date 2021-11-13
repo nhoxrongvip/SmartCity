74926b568869e0a45e219a05333e373ce1c5413a
chudanghieu <kunzhieu1999@gmail.com>
2021-11-12 21:23:55 -0500

    Add blinking function to the ALERT box

--- SmartCity/src/main/java/ca/chesm/it/smartcity/SnowLevelFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/SnowLevelFragment.java
@@ -4,12 +4,20 @@ package ca.chesm.it.smartcity;
 //        Course: CENG322-RND
 //        Purpose: Control Snow level on street
 //        Last updated: Sep 27 2021
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +25,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class SnowLevelFragment extends Fragment {
+
+    TextView snow_alert;
+    Button snow_alertBtn;
+    View v;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,12 +69,36 @@ public class SnowLevelFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_snow_level, container, false);
+        v = inflater.inflate(R.layout.fragment_snow_level, container, false);
+
+        snow_alert = (TextView) v.findViewById(R.id.Snow_Alert);
+        snow_alertBtn = (Button) v.findViewById(R.id.Snow_AlertBtn);
+        snow_alertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlinkEffect();
+            }
+        });
+
+        return v;
+    }
+
+    @SuppressLint("WrongConstant")
+    public void BlinkEffect(){
+        ObjectAnimator anim = ObjectAnimator.ofInt(snow_alert, "backgroundColor", Color.WHITE, Color.RED, Color.WHITE);
+        anim.setDuration(800);
+        anim.setEvaluator(new ArgbEvaluator());
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.start();
     }
 }
