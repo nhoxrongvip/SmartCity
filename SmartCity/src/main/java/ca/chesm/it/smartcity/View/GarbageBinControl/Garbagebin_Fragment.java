62b4b75d6fd8237705cc4d7590095ef78cd7a850
Danny <danks76429@gmail.com>
2021-11-29 13:52:38 -0500

    still need to update random data for bin Garbagebin_Fragment commit

--- SmartCity/src/main/java/ca/chesm/it/smartcity/View/GarbageBinControl/Garbagebin_Fragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/View/GarbageBinControl/Garbagebin_Fragment.java
@@ -1,8 +1,11 @@
 package ca.chesm.it.smartcity.View.GarbageBinControl;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,10 +17,15 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
@@ -32,10 +40,12 @@ public class Garbagebin_Fragment extends Fragment
     private Runnable runnable;
     private TextView txtaddress, txtbattery, txtbinname;
     private Button orgabin_btn, recylebin_btn, garbagebin_btn;
+    SharedPreferences sharedPreferences;
+    private String binname;
     private ProgressBar progressBar;
     private City city;
-    private Thread thread;
-    private ExecutorService pool;
+    private Timer timer;
+    private boolean runb;
 
     public Garbagebin_Fragment()
     {
@@ -54,7 +64,13 @@ public class Garbagebin_Fragment extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
+        sharedPreferences = this.getActivity().getSharedPreferences("SmartCity", Context.MODE_PRIVATE);
+        runb = sharedPreferences.getBoolean("Task Run", false);
         v = inflater.inflate(R.layout.garbagebin_fragment, container, false);
+        if(runb == true)
+        {
+            timer.cancel();
+        }
         Bundle bundle = this.getArguments();
         setupid();
         if (bundle != null)
@@ -64,9 +80,23 @@ public class Garbagebin_Fragment extends Fragment
         city = (City) bundle.get("city");
         ProgressbarBin progressbarBin = new ProgressbarBin();
         progressbarBin.execute();
+
         txtaddress.setText(city.getAddress());
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Garbage").child("City").child(city.getName()).child(String.valueOf(city.getId()));
+        ref.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
 
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+
+            }
+        });
         return v;
     }
 
@@ -79,31 +109,51 @@ public class Garbagebin_Fragment extends Fragment
         return radNum;
     }
 
+    private void binupdate(String name)
+    {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Garbage").child("City").child(city.getName()).child(String.valueOf(city.getId()));
+        double temp = 0;
+        String rad = String.format("%.2f", rndDataforBin());
+        String rad2 = String.format("%.2f", rndDataforBin());
+        String rad3 = String.format("%.2f", rndDataforBin());
+        double number = Double.parseDouble(rad);
+        double number1 = Double.parseDouble(rad2);
+        double number2 = Double.parseDouble(rad3);
+        ref.child(garbagebin_btn.getText().toString()).setValue(number);
+        ref.child(recylebin_btn.getText().toString()).setValue(number1);
+        ref.child(garbagebin_btn.getText().toString()).setValue(number2);
+        city.setBin1(number);
+        ProgressBarAnimation anim = new ProgressBarAnimation(progressBar, temp, city.getBin1());
+        anim.setDuration(1000);
+        progressBar.startAnimation(anim);
+    }
+
+    public class TestClass extends TimerTask{
+
+        public void doStuff()
+        {
+            binupdate(binname);
+        }
+
+        @Override
+        public void run()
+        {
+            if(runb == false)
+            {
+                doStuff();
+            }
+        }
+
+    }
+
+
     private class ProgressbarBin extends AsyncTask<Void, Void, Void>
     {
 
         @Override
         protected Void doInBackground(Void... voids)
         {
-            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Garbage").child("City").child(city.getName()).child(String.valueOf(city.getId()));
-            try
-            {
-                while (true)
-                {
-                    Thread.sleep(10000);
-                    double temp = city.getBin1();
-                    String rad = String.format("%.2f",rndDataforBin());
-                    double number = Double.parseDouble(rad);
-                    ref.child("bin 1").setValue(number);
-                    city.setBin1(number);
-                    ProgressBarAnimation anim = new ProgressBarAnimation(progressBar, temp, city.getBin1());
-                    anim.setDuration(1000);
-                    progressBar.startAnimation(anim);
-                }
-            } catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
+
             return null;
         }
 
@@ -141,7 +191,8 @@ public class Garbagebin_Fragment extends Fragment
         protected void onPreExecute()
         {
             super.onPreExecute();
-            txtbinname.setText(orgabin_btn.getText().toString());
+            binname = orgabin_btn.getText().toString();
+            txtbinname.setText(binname);
             ProgressBarAnimation anim = new ProgressBarAnimation(progressBar, 0, city.getBin1());
             anim.setDuration(1000);
             progressBar.startAnimation(anim);
@@ -173,6 +224,12 @@ public class Garbagebin_Fragment extends Fragment
             txtbattery.setText(String.format("%.2f", value) + "%");
         }
 
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
     }
 
     private void setupid()
