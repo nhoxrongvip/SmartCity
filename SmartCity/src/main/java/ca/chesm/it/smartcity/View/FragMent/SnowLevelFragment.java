07ae130ea64acfa5cd40c0f8b846e1e5d2fa4245
chudanghieu <kunzhieu1999@gmail.com>
2021-11-29 17:22:27 -0500

    Remove snow simulator, now using API to pass snow level into the app Temperature and Humidity API is now added into the app Geography location is now added and using default location

--- SmartCity/src/main/java/ca/chesm/it/smartcity/View/FragMent/SnowLevelFragment.java
+++ SmartCity/src/main/java/ca/chesm/it/smartcity/View/FragMent/SnowLevelFragment.java
@@ -7,7 +7,12 @@ package ca.chesm.it.smartcity.View.FragMent;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
@@ -20,11 +25,26 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Timer;
-import java.util.TimerTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Locale;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import ca.chesm.it.smartcity.R;
 import de.hdodenhof.circleimageview.CircleImageView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,10 +58,16 @@ public class SnowLevelFragment extends Fragment {
     Button snow_alertBtn;
 
     Spinner snow_location;
-    TextView snow_level;
+    TextView snow_lv, temperature, humidity;
 
     CircleImageView snow_weather_image;
+    //Location data
+    double longitude, latitude;
 
+    SharedPreferences sharedPreferences;
+    private String city_name = "";
+    private String url;
+    public String humid, snow, temp;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -50,6 +76,7 @@ public class SnowLevelFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String APIkey = "fb3bee783ff014198af717516379bfe1";
 
     public SnowLevelFragment() {
         // Required empty public constructor
@@ -93,8 +120,8 @@ public class SnowLevelFragment extends Fragment {
         v = inflater.inflate(R.layout.fragment_snow_level, container, false);
 
         //Blinking effect for Alert button
-        snow_alert = (TextView) v.findViewById(R.id.Snow_Alert);
-        snow_alertBtn = (Button) v.findViewById(R.id.Snow_AlertBtn);
+        snow_alert = v.findViewById(R.id.Snow_Alert);
+        snow_alertBtn = v.findViewById(R.id.Snow_AlertBtn);
         snow_alertBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -105,6 +132,13 @@ public class SnowLevelFragment extends Fragment {
         //Change weather image based on weather
         snow_weather_image = v.findViewById(R.id.snowLevelImage);
 
+        //Text View temp and humid
+        temperature = v.findViewById(R.id.Snow_temperature);
+        humidity = v.findViewById(R.id.Snow_humidity);
+        snow_lv = v.findViewById(R.id.snow_level);
+
+
+
         //Spinner location
         snow_location = v.findViewById(R.id.Snow_currentlocation);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
@@ -112,26 +146,33 @@ public class SnowLevelFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         snow_location.setAdapter(adapter);
 
-        timer.schedule(snowlvTask, 10, 5000);
 
-        //Image View change due to the change in snow level
-        snow_weather_image= v.findViewById(R.id.snowLevelImage);
-        String snow_result = snowlvCheck();
-        switch (snow_result)
-        {
-            case "low":
-                snow_weather_image.setImageDrawable(getResources().getDrawable(R.drawable.snow));
-                break;
-            case "medium":
-                snow_weather_image.setImageDrawable(getResources().getDrawable(R.drawable.snowmedium));
-                break;
-            case "high":
-                snow_weather_image.setImageDrawable(getResources().getDrawable(R.drawable.snowheavy));
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + snow_result);
+        sharedPreferences = this.getActivity().getSharedPreferences("SmartCity", Context.MODE_PRIVATE);
+        longitude = Double.parseDouble(sharedPreferences.getString("longitude", "0.0"));
+        latitude = Double.parseDouble(sharedPreferences.getString("latitude", "0.0"));
+
+        longitude = Double.parseDouble(sharedPreferences.getString("longitude", "0.0"));
+        latitude = Double.parseDouble(sharedPreferences.getString("latitude", "0.0"));
+
+        //Get default location
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 3);
+            city_name = addresses.get(0).getLocality();
+            int spinnerPosition = adapter.getPosition(city_name);
+            snow_location.setSelection(spinnerPosition);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        url = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=" + APIkey;
+        ReadJSONFeed feed = new ReadJSONFeed();
+        feed.execute(url);
 
         //Returns
         return v;
@@ -139,7 +180,7 @@ public class SnowLevelFragment extends Fragment {
 
 
     @SuppressLint("WrongConstant")
-    public void BlinkEffect(){
+    public void BlinkEffect() {
         ObjectAnimator anim = ObjectAnimator.ofInt(snow_alert, "backgroundColor", Color.WHITE, Color.RED, Color.WHITE);
         anim.setDuration(800);
         anim.setEvaluator(new ArgbEvaluator());
@@ -148,42 +189,99 @@ public class SnowLevelFragment extends Fragment {
         anim.start();
     }
 
-    public int radNum = 0;
-    class randNoGenerator extends TimerTask {
-        public void run() {
-            //Do something here please I am stupid
-            //random no generator
-            int min = 0;
-            int max = 25;
-            radNum = (int)(Math.random() * (double)(max - min + 1) + (double)min);
+    public void setSnowTHView() {
+        temperature.setText(temp);
+        humidity.setText(humid);
+        snow_lv.setText(snow);
+    }
 
-            //Snow level simulator
-            snow_level = v.findViewById(R.id.snow_level);
-            snow_level.setText(radNum + " " + getString(R.string.unitCM));
+    private class ReadJSONFeed extends AsyncTask<String, Void, String> {
+
+        public ReadJSONFeed() {
 
         }
-    }
-    //Task timer
-    randNoGenerator snowlvTask = new randNoGenerator();
-    Timer timer = new Timer();
 
-    //Snow level checking level
-    public String snowlvCheck() {
-        try {
-            int value = radNum;
-            if (value >= 0 && value < 8) {
-                return "low";
-            } else if (value >= 8 && value < 16) {
-                return "medium";
-            } else if (value > 16 && value <= 25) {
-                return "high";
+        @Override
+        protected String doInBackground(String... urls) {
+            return readJSON(urls[0]);
+        }
+
+        private String readJSON(String address) {
+            URL url = null;
+            StringBuilder sb = new StringBuilder();
+            HttpsURLConnection urlConnection = null;
+            try {
+                //Get URL
+                url = new URL(address);
+                //Open URL
+                urlConnection = (HttpsURLConnection) url.openConnection();
+                InputStream content = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+            }
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                JSONObject dataObject = new JSONObject(result);
+                JSONObject dataMain = dataObject.getJSONObject("main");
+
+                if (dataObject.has("snow")) {
+                    JSONObject dataSnow = dataObject.getJSONObject("snow");
+                    getSnow(dataSnow);
+                } else {
+                    snow = "0";
+                }
+
+                getTemperature(dataMain);
+                getHumidity(dataMain);
+                setSnowTHView();
+
+
+            } catch (JSONException jsonException) {
+                jsonException.printStackTrace();
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+
         }
-        return "Not in range";
+
+        public void getTemperature(JSONObject dataObject) {
+            try {
+                DecimalFormat df = new DecimalFormat("0.00");
+                temp = df.format(Double.parseDouble(dataObject.getString("temp")) - 273.15);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void getHumidity(JSONObject dataObject) {
+            try {
+                humid = dataObject.getString("humidity");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void getSnow(JSONObject dataObject) {
+            try {
+                snow = dataObject.getString("1h");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
 }
